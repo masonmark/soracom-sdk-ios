df28d5af15777ad51e0c82a17237f6eafd1b43c8
Mason Mark <mason@fivespeed.com>
2016-02-17 17:59:45 +0900

    Fix "Mason 2016-02-16: PARSING AUTH RESULT ALWAYS FAILS" that prevented login from working.
    There was a bug here where the code to handle success never executed, because response.result.value was conditionally cast to [String: String] and that cast always failed. Changing [String: String]  to NSDictionary fixed it. I presume (but didn't prove) that AlamoFire changed something about Response that caused that old code to no longer work. Anyway, I think asserting we have a valid dictionary is sufficient for this purpose.
    
    It might be better at some point to change this to something like:
    
    switch response.result {
    case .Success(let JSON):
        hooray()
    case .Failure(let error):
        booHoo()
    }
    
    ...but for now I am making just the minimal possible change.

--- Pod/Classes/AuthAPI.swift
+++ Pod/Classes/AuthAPI.swift
@@ -11,34 +11,11 @@ extension Soracom {
                 if let error = response.result.error {
                     onError?(error)
                 } else {
-                    if let value = response.result.value as? [String: String],
-                        let apiKey = value["apiKey"],
-                        let token = value["token"],
-                        let operatorId = value["operatorId"]
-                    {
-                        // Mason 2016-02-16: PARSING AUTH RESULT ALWAYS FAILS
-                        // This code never executes in my limited testing, 
-                        // because value is always nil here.
-                        //
-                        // In lldb it looks like:
-                        //
-                        //    (lldb) po response.result.value as? [String: String]
-                        //    nil
-                        //
-                        // ...even though `result` is a valid AlamoFire.Response (.Success) and
-                        //
-                        //    (lldb) po response.result.value as? NSDictionary
-                        //
-                        // ...prints something that looks valid.
-                        //
-                        // So it looks like the conditional type cast 
-                        // to [String: String] always fails. I think probably 
-                        // something changed in AlamoFire since original code was written?
-                        // 
-                        // End result is that app hangs at the login screen after login
-                        // attempt (since the onSuccess?() call never happens) and just
-                        // prints response.result.value) to console.
-
+                  if let value = response.result.value as? NSDictionary,
+                    apiKey     = value["apiKey"] as? String,
+                    token      = value["token"] as? String,
+                    operatorId = value["operatorId"] as? String
+                  {
                         Router.token = token
                         Router.apiKey = apiKey
                         Router.operatorId = operatorId
