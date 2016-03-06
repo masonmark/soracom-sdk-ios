146096a299c6392d40a381a0f382ece4f1d5fd8c
Mason Mark <mason@fivespeed.com>
2016-03-06 07:39:13 -0500

    Document issue "Failures in auth() may not be reported correctly (Mason 2016-03-06)"

--- Pod/Classes/AuthAPI.swift
+++ Pod/Classes/AuthAPI.swift
@@ -21,6 +21,18 @@ extension Soracom {
                         Router.operatorId = operatorId
                         onSuccess?()
                     } else {
+                      // FIXME: Failures in auth() may not be reported correctly (Mason 2016-03-06)
+                      // Need to report error here. At least one (and perhaps all?) errors will look like this:
+                      //      (lldb) po response.result
+                      //        ▿ SUCCESS: {
+                      //          code = COM0008;
+                      //          message = "Constraint vaiolation of input value. message:not a well-formed email address:email";
+                      //      }
+                      //
+                      // So, either a.) the Soracom API is not returning an HTTP error, and instead returns an HTTP success response with the error expressed in the JSON structure, or b.) something else (haha).
+                      //
+                      // I'm in an airplane so it's not easy to check. Either way, though, we need to improve this function to reliably indicate error, and invoke onError(), in all error cases.
+
                         print(response.result.value)
                     }
                 }
