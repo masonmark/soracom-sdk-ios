29af39a995ecb6eb2c79f20350d031bd1449edd8
Mason Mark <mason@fivespeed.com>
2016-03-12 17:12:52 +0900

    🔥Burn it all down. I think my fork is end-of-life now, in favor of soracom-sdk-swift.

--- Pod/Classes/AuthAPI.swift
+++ Pod/Classes/AuthAPI.swift
@@ -7,61 +7,61 @@ extension Soracom {
 
     /// Mason 2016-03-06: this is my experimental thing. STILL YOUNG AND NOT FINISHED!
     
-    public class func authenticate(credentials: SoracomAccountCredentials, resultHandler: ((SoracomAPIResult) -> ()) ) {
-        
-        let timeout = 86400
-        let requestValues: [String: AnyObject]
-        
-        if credentials.type == .RootAccount {
-            requestValues = [
-                "email"               : credentials.emailAddress,
-                "password"            : credentials.password,
-                "tokenTimeoutSeconds" : timeout
-            ]
-        } else if credentials.type == .SAM {
-            requestValues = [
-                "operatorId"          : credentials.operatorID,
-                "username"            : credentials.username,
-                "password"            : credentials.password,
-                "tokenTimeoutSeconds" : timeout
-            ]
-        } else {
-            fatalError("FIXME: unimplemented: auth type other than RootAccount or SAM. ")
-        }
-        
-        Alamofire.request(Router.Auth(requestValues)).responseJSON { response in
-            
-            // Mason 2016-03-06: FIXME: This is temporary, experimental code!
-            
-            let HTTPStatus  = response.response?.statusCode
-            let payload     = response.result.value as? Dictionary<String, AnyObject>
-            var result      = SoracomAPIResult(HTTPStatus: HTTPStatus, payload: payload)
-            
-            print("API レスポンス bro：")
-            print(result)
-            
-            // Mason 2016-03-06: My experimentation has so far revealed different failure modes:
-            
-            if let error = response.result.error {
-                // Failure mode 1: network offline, or other underlying Cocoa error occurred. In this case, response.response may be nil.
-                result.clientError = error
-            } else if result.hasError {
-                // do nothing, err is already set
-            } else {
-                if let value = response.result.value as? NSDictionary,
-                    apiKey     = value["apiKey"] as? String,
-                    token      = value["token"] as? String,
-                    operatorId = value["operatorId"] as? String
-                {
-                    Router.token = token
-                    Router.apiKey = apiKey
-                    Router.operatorId = operatorId
-                }
-            }
-            
-            resultHandler(result)
-        }
-    }
+//    public class func authenticate(credentials: SoracomAccountCredentials, resultHandler: ((SoracomAPIResult) -> ()) ) {
+//        
+//        let timeout = 86400
+//        let requestValues: [String: AnyObject]
+//        
+//        if credentials.type == .RootAccount {
+//            requestValues = [
+//                "email"               : credentials.emailAddress,
+//                "password"            : credentials.password,
+//                "tokenTimeoutSeconds" : timeout
+//            ]
+//        } else if credentials.type == .SAM {
+//            requestValues = [
+//                "operatorId"          : credentials.operatorID,
+//                "username"            : credentials.username,
+//                "password"            : credentials.password,
+//                "tokenTimeoutSeconds" : timeout
+//            ]
+//        } else {
+//            fatalError("FIXME: unimplemented: auth type other than RootAccount or SAM. ")
+//        }
+//        
+//        Alamofire.request(Router.Auth(requestValues)).responseJSON { response in
+//            
+//            // Mason 2016-03-06: FIXME: This is temporary, experimental code!
+//            
+//            let HTTPStatus  = response.response?.statusCode
+//            let payload     = response.result.value as? Dictionary<String, AnyObject>
+//            var result      = SoracomAPIResult(HTTPStatus: HTTPStatus, payload: payload)
+//            
+//            print("API レスポンス bro：")
+//            print(result)
+//            
+//            // Mason 2016-03-06: My experimentation has so far revealed different failure modes:
+//            
+//            if let error = response.result.error {
+//                // Failure mode 1: network offline, or other underlying Cocoa error occurred. In this case, response.response may be nil.
+//                result.clientError = error
+//            } else if result.hasError {
+//                // do nothing, err is already set
+//            } else {
+//                if let value = response.result.value as? NSDictionary,
+//                    apiKey     = value["apiKey"] as? String,
+//                    token      = value["token"] as? String,
+//                    operatorId = value["operatorId"] as? String
+//                {
+//                    Router.token = token
+//                    Router.apiKey = apiKey
+//                    Router.operatorId = operatorId
+//                }
+//            }
+//            
+//            resultHandler(result)
+//        }
+//    }
     
     
     public class func auth(email: String,
